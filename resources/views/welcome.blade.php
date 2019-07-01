2b11e4e813871a688a4cac7b13a5976b096b87e5
Hoang Tuan <hoangtuan51096@gmail.com>
2019-07-01 14:39:51 +0700

    hehe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -97,7 +97,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                <h2> Hello  </h2>
+                <h2> Hello baby </h2>
                 </div>
             </div>
         </div>
